8c9951ef0f9fcc83075d9edc507d6b18e6dd7d2a
yuanguangji <1351527793@163.com>
2025-07-31 00:44:39 +0800

    1

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,7 +2,10 @@ declare module '*.svg' {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
-
+declare module '*.svg?url' {
+    const src: string;
+    export default src;
+  }
 declare module '*.less' {
     const classes: { [className: string]: string };
     export default classes;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+declare module '*.svg?url' {
+    const src: string;
+    export default src;
+  }
